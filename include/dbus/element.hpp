d2831c0121aea0d5facd61af47348f2e100cdf76
Benjamin Kietzman <bengilgit@gmail.com>
2014-06-17 23:23:05 -0700

    added typedefs and codes for more message elements

--- include/dbus/element.hpp
+++ include/dbus/element.hpp
@@ -15,18 +15,57 @@ namespace dbus {
 /**
  * D-Bus Messages are composed of simple elements of one of these types
  */
+// bool // is this simply valid? It might pack wrong...
+        // http://maemo.org/api_refs/5.0/5.0-final/dbus/api/group__DBusTypes.html
+typedef unsigned char byte;
+typedef short int16;
+typedef unsigned short uint16;
+typedef int int32;
+typedef unsigned int uint32;
+typedef long long int64;
+typedef unsigned long long uint64;
+// double
+// unix_fd
+
 typedef std::string   string;
-typedef dbus_int32_t  int32;
-typedef dbus_uint32_t uint32;
+struct object_path
+{
+  string value;
+};
+struct signature
+{
+  string value;
+};
+
 //TODO: add more types...
 
 /// Traits template for message elements
 /**
  * D-Bus Message elements are identified by unique integer type codes.
  */
-template<typename InvalidType=void> struct element
+template<typename InvalidType> struct element
 {
   static const int code = DBUS_TYPE_INVALID;
+};
+
+template<> struct element< bool >
+{
+  static const int code = DBUS_TYPE_BOOLEAN;
+};
+
+template<> struct element< byte >
+{
+  static const int code = DBUS_TYPE_BYTE;
+};
+
+template<> struct element< int16 >
+{
+  static const int code = DBUS_TYPE_INT16;
+};
+
+template<> struct element< uint16 >
+{
+  static const int code = DBUS_TYPE_UINT16;
 };
 
 template<> struct element< int32 >
@@ -39,20 +78,113 @@ template<> struct element< uint32 >
   static const int code = DBUS_TYPE_UINT32;
 };
 
+template<> struct element< int64 >
+{
+  static const int code = DBUS_TYPE_INT64;
+};
+
+template<> struct element< uint64 >
+{
+  static const int code = DBUS_TYPE_UINT64;
+};
+
+template<> struct element< double >
+{
+  static const int code = DBUS_TYPE_DOUBLE;
+};
+
 template<> struct element< string >
 {
   static const int code = DBUS_TYPE_STRING;
 };
 
-/// Convenience overloads
-template<> struct element< char * >
+template<> struct element< object_path >
 {
-  static const int code = DBUS_TYPE_STRING;
+  static const int code = DBUS_TYPE_OBJECT_PATH;
 };
-template<> struct element< const char * >
+
+template<> struct element< signature >
 {
-  static const int code = DBUS_TYPE_STRING;
+  static const int code = DBUS_TYPE_SIGNATURE;
 };
+
+
+
+template<typename InvalidType> struct is_fixed_type
+{
+  static const int value = false;
+};
+
+template<> struct is_fixed_type< bool >
+{
+  static const int value = true;
+};
+
+template<> struct is_fixed_type< byte >
+{
+  static const int value = true;
+};
+
+template<> struct is_fixed_type< int16 >
+{
+  static const int value = true;
+};
+
+template<> struct is_fixed_type< uint16 >
+{
+  static const int value = true;
+};
+
+template<> struct is_fixed_type< int32 >
+{
+  static const int value = true;
+};
+
+template<> struct is_fixed_type< uint32 >
+{
+  static const int value = true;
+};
+
+template<> struct is_fixed_type< int64 >
+{
+  static const int value = true;
+};
+
+template<> struct is_fixed_type< uint64 >
+{
+  static const int value = true;
+};
+
+template<> struct is_fixed_type< double >
+{
+  static const int value = true;
+};
+
+
+
+template<typename InvalidType> struct is_string_type
+{
+  static const bool value = false;
+};
+
+template<> struct is_string_type< string >
+{
+  static const bool value = true;
+};
+
+template<> struct is_string_type< object_path >
+{
+  static const bool value = true;
+};
+
+template<> struct is_string_type< signature >
+{
+  static const bool value = true;
+};
+
+
+
+
 
 } // namespace dbus
 
