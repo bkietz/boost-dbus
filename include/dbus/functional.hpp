2e15977f5f00d6bc32d8e80aea155923efcde673
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2015-01-29 18:46:56 -0800

    Use C++11 std::function

--- include/dbus/functional.hpp
+++ include/dbus/functional.hpp
@@ -6,27 +6,9 @@
 #ifndef DBUS_FUNCTIONAL_HPP
 #define DBUS_FUNCTIONAL_HPP
 
-/*
-#ifndef BOOST_NO_CXX11_HDR_FUNCTIONAL
 #include <functional>
 namespace dbus {
 using std::function;
 }
-#else
-#include <boost/tr1/functional.hpp>
-namespace dbus {
-using std::tr1::function;
-}
-#endif
-*/
-
-// The above was not working because of
-// reference_wrapper being declared in std::tr1.
-// I suspect this is a bug in BOOST_HAS_TR1_REFERENCE_WRAPPER
-#include <boost/function.hpp>
-namespace dbus {
-using boost::function;
-}
-
 
 #endif // DBUS_FUNCTIONAL_HPP
